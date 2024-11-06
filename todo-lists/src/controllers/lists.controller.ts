1a0eb3038a4e561efdb0e1059535e320a7497193
AIT BENMESSAOUD Amine <amineabm52002@gmail.com>
2024-11-06 11:41:52 +0100

    initialize project structure with LevelDB integration and basic schemas

---
+++ todo-lists/src/controllers/lists.controller.ts
@@ -0,0 +1,35 @@
+import { FastifyReply, FastifyRequest } from "fastify"
+import { ITodoList } from "../interfaces"
+
+let staticLists: ITodoList[] = [
+  {
+	id: '1',
+    name: 'Shopping List',      
+  },
+]
+
+export async function listLists(
+  request: FastifyRequest, 
+  reply: FastifyReply
+) {
+  console.log('DB status', this.level.db.status)
+  const listsIter = this.level.db.iterator()
+
+  const result: ITodoList[] = []
+  for await (const [key, value] of listsIter) {
+    result.push(JSON.parse(value))
+  }
+  reply.send({ data: result })
+}
+
+
+export async function addList(
+  request: FastifyRequest, 
+  reply: FastifyReply
+) {
+ const list = request.body as ITodoList
+ const result = await this.level.db.put(
+   list.id.toString(), JSON.stringify(list)
+ )
+ reply.send({ data: result })
+}
