2fd023cd9b6343fd0d0e45e8d986aa67621b3d14
Ewwweee <gabriel.bocquet@imt-atlantique.net>
2024-11-06 12:03:17 +0100

    Mes modifs

--- todo-lists/src/interfaces/index.ts
+++ todo-lists/src/interfaces/index.ts
@@ -1,4 +1,19 @@
-export interface ITodoList {
-    id: string
-    name: string
-  }
+interface ITodoList {
+  id: string;
+  name: string;
+  description: string;
+  task: Task[];
+}
+
+interface Task {
+  id: string;
+  name: string;
+  description: string;
+  importance: number;
+}
+
+interface IdInURL {
+  id: number;
+}
+
+export type { ITodoList, IdInURL, Task };
--- todo-lists/src/routes/lists/index.ts
+++ todo-lists/src/routes/lists/index.ts
@@ -1,13 +1,14 @@
-import { FastifyInstance } from 'fastify'
-import * as listsController from '../../controllers/lists.controller'
+import { FastifyInstance } from "fastify";
+import * as listsController from "../../controllers/lists.controller";
 
 async function lists(fastify: FastifyInstance) {
-
-  fastify.get('/', listsController.listLists)
+  fastify.get("/", listsController.listLists);
 
   // TODO implement addList in controller
-  fastify.post('/', listsController.addList)
+  fastify.post("/", listsController.addList);
 
+  fastify.put("/:id", listsController.changeElem);
+  fastify.delete("/:id/item/:id", listsController.delElem);
 }
 
-export default lists
+export default lists;
